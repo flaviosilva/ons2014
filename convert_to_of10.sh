9895a09903e28601f3da697c74f6d519dfa48cdb
Luis Chiang <luis@chiang.ec>
2014-03-02 17:00:32 -0800

    final hackaton

---
+++ convert_to_of10.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo ovs-vsctl set bridge s1 protocols=OpenFlow10
+
--- run_mininet.sh
+++ run_mininet.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo mn --controller=remote --ip=127.0.0.1 --port=6633
+sudo mn --controller=remote,ip=127.0.0.1,port=6633 --topo=single,4
 
